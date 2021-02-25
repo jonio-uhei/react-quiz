df7c85e401bd231fbddba6e203ccdffb6f81a697
jonio-uhei <hayata19791218@yahoo.co.jp>
2021-02-25 13:03:06 +0900

    データのアップロード

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react';
+import Answer from './components/Answer';
 
 function App() {
+
+  const questions =[
+    {
+      questionText:'数学Ⅲで一番難しい分野は',
+      answerOptions:[
+        {answerText:'微分積分',isCorrect:true},
+        {answerText:'ベクトル',isCorrect:false},
+        {answerText:'2次曲線',isCorrect:false},
+      ],
+    },
+    {
+      questionText:'この中で難易度が低いプログラミング言語は',
+      answerOptions:[
+        {answerText:'React.js',isCorrect:false},
+        {answerText:'Angular.js',isCorrect:false},
+        {answerText:'HTML',isCorrect:true},
+      ],
+    },
+    {
+      questionText:'この中でYouTuberのゲーム実況者は',
+      answerOptions:[
+        {answerText:'Taiman',isCorrect:false},
+        {answerText:'ガッチマン',isCorrect:true},
+        {answerText:'ハクア',isCorrect:false},
+      ],
+    },
+  ];
+
+  const [currentQuestion,setCurrentQuestion] = useState(0);
+
+  const [showScore,setShowScore] = useState(false);
+
+  const [score,setScore] = useState(0);
+
+  const handleAnswerButtonClick = (isCorrect) => {
+
+    if(isCorrect === true){
+      alert('正解です');
+      setScore(score+1);
+    }else{
+      alert('不正解です');
+    }
+
+    const nextQuestion = currentQuestion + 1;
+
+    if(nextQuestion < questions.length){
+      setCurrentQuestion(nextQuestion);
+    }else{
+      setShowScore(true);
+
+    }
+  }
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        showScore ? (
+          <p>お疲れ様でした!<br></br><span class="correct">3問中{score}問</span>正解です</p>
+        )
+        :
+        (
+          <Answer
+            handleAnswerButtonClick={handleAnswerButtonClick}
+            questions={questions}
+            currentQuestion={currentQuestion}
+          />
+        )
+      }
     </div>
   );
 }
---
+++ src/components/Answer.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+    const Answer = ({handleAnswerButtonClick,questions,currentQuestion}) =>{
+        return(
+            <>
+                <h1>3択クイズ</h1>
+                <h2><span>第1問</span><br></br>問題:{questions[currentQuestion].questionText}</h2>
+                <ul> 
+                {
+                    questions[currentQuestion].answerOptions.map((answerOption,key)=>(
+                        <li 
+                            key={key}
+                            onClick={()=>handleAnswerButtonClick(answerOption.isCorrect)}>{answerOption.answerText}
+                        </li>)
+                    )
+                }
+                </ul>
+            </>
+        );
+    };
+
+export default Answer;
