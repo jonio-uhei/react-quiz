de17094c33f85d024c7297edf356185ca0cdf274
jonio-uhei <hayata19791218@yahoo.co.jp>
2021-02-26 13:50:17 +0900

    Answer.jsxの修正

--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -4,7 +4,7 @@ import React from 'react';
         return(
             <>
                 <h1>3択クイズ</h1>
-                <h2><span>第1問</span><br></br>問題:{questions[currentQuestion].questionText}</h2>
+        <h2><span>第{currentQuestion+1}問</span><br></br>問題:{questions[currentQuestion].questionText}</h2>
                 <ul> 
                 {
                     questions[currentQuestion].answerOptions.map((answerOption,key)=>(
